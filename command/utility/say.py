504bbe4780e412773a806e3ffebfcf3858fac05a
Artic <81034458+ArticOff@users.noreply.github.com>
2022-07-08 20:16:50 +0200

    Update say.py

--- command/utility/say.py
+++ command/utility/say.py
@@ -1,12 +1,12 @@
 from importlib import machinery
-discord_kairo = machinery.SourceFileLoader('discord_kairo','discord_kairo.py').load_module()
+discord_akerno = machinery.SourceFileLoader('discord_akerno','discord_akerno.py').load_module()
 
-async def _say(bot, message: discord_kairo.ext.Message):
+async def _say(bot, message: discord_akerno.ext.Message):
     await message.delete()
     await message.channel.send(str(message.content).removeprefix('!say '))
 
 if __name__ == '__main__':
-    say = discord_kairo.Command
+    say = discord_akerno.Command
     say.constructor(say, 'say', 'utility', 'Say', ['dire'])
     say.execute(say, _say)
     say.Push(say)
