95d9e8d3a1b6c1b60bd0439dba62c9add55f72a6
Artic <81034458+ArticOff@users.noreply.github.com>
2022-07-08 20:15:47 +0200

    Update ban.py

--- command/moderation/ban.py
+++ command/moderation/ban.py
@@ -1,13 +1,13 @@
 from importlib import machinery
-discord_kairo = machinery.SourceFileLoader('discord_kairo','discord_kairo.py').load_module()
+discord_akerno = machinery.SourceFileLoader('discord_akerno','discord_akerno.py').load_module()
 
-async def _ban(bot, message: discord_kairo.ext.Message):
+async def _ban(bot, message: discord_akerno.ext.Message):
     user = message.mentions[0]
     await message.channel.send(f'{user.mention} has been banned for `{" ".join(str(message.content).split()[2:])}`!')
     await user.ban(reason=" ".join(str(message.content).split()[2:]))
 
 if __name__ == '__main__':
-    ban = discord_kairo.Command
+    ban = discord_akerno.Command
     ban.constructor(ban, 'ban', 'moderation', 'Ban', ['degage'])
     ban.execute(ban, _ban)
     ban.Push(ban)
