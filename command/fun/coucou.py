8892b33d90b20cbc21cbdc5983fbeb300c6e196b
Artic <81034458+ArticOff@users.noreply.github.com>
2022-07-08 22:18:07 +0200

    Add files via upload

---
+++ command/fun/coucou.py
@@ -0,0 +1,12 @@
+
+from importlib import machinery
+discord_akerno = machinery.SourceFileLoader('discord_akerno','discord_akerno.py').load_module()
+
+async def _coucou(bot ,message: discord_akerno.ext.Message):
+    await message.reply('Coucou ! :wave:')
+
+if __name__ == '__main__':
+    coucou = discord_akerno.Command
+    coucou.constructor(coucou, "coucou", "fun", "hey", ['salut', 'hey'])
+    coucou.execute(coucou, _coucou)
+    coucou.Push(coucou)
