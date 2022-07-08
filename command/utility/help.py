ad2bed7c16cbe6aa7d7c7809a8c59e8a5afcf823
Artic <81034458+ArticOff@users.noreply.github.com>
2022-07-08 20:16:39 +0200

    Update help.py

--- command/utility/help.py
+++ command/utility/help.py
@@ -1,13 +1,13 @@
 import json, discord
 
 from importlib import machinery
-discord_kairo = machinery.SourceFileLoader('discord_kairo','discord_kairo.py').load_module()
+discord_akerno = machinery.SourceFileLoader('discord_akerno','discord_akerno.py').load_module()
 
-async def _help(bot, message: discord_kairo.ext.Message):
+async def _help(bot, message: discord_akerno.ext.Message):
     await message.delete()
     if len(str(message.content).split()) > 1:
         category = str(message.content).split()[1]
-        with open('kairo.commands.json', 'r') as f:
+        with open('akerno.commands.json', 'r') as f:
             try:
                 commands = json.load(f)
                 embed = discord.Embed(title=f'help [{"".join(category)}]', description='Liste des commandes', color=discord.Color.blue())
@@ -20,8 +20,7 @@ async def _help(bot, message: discord_kairo.ext.Message):
         await message.channel.send(f'```{message.content} moderation\n{message.content} utility\n{message.content} fun```')
 
 if __name__ == '__main__':
-    help = discord_kairo.Command
+    help = discord_akerno.Command
     help.constructor(help, "help", "utility", "The help command", ['help'])
     help.execute(help, _help)
     help.Push(help)
-    
