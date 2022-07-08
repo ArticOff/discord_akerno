c5b7ecd2051ff89d60b78903f993515d58a9976e
Artic <81034458+ArticOff@users.noreply.github.com>
2022-07-08 20:15:59 +0200

    Update kick.py

--- command/moderation/kick.py
+++ command/moderation/kick.py
@@ -1,13 +1,13 @@
 from importlib import machinery
-discord_kairo = machinery.SourceFileLoader('discord_kairo','discord_kairo.py').load_module()
+discord_akerno = machinery.SourceFileLoader('discord_akerno','discord_akerno.py').load_module()
 
-async def _kick(bot, message: discord_kairo.ext.Message):
+async def _kick(bot, message: discord_akerno.ext.Message):
     user = message.mentions[0]
     await message.channel.send(f'{user.mention} has been kicked for `{" ".join(str(message.content).split()[2:])}`!')
     await user.kick(reason=" ".join(str(message.content).split()[2:]))
 
 if __name__ == '__main__':
-    kick = discord_kairo.Command
+    kick = discord_akerno.Command
     kick.constructor(kick, 'kick', 'moderation', 'Kick', ['expulser'])
     kick.execute(kick, _kick)
     kick.Push(kick)
