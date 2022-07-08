7ee8a01dc7eb87367468a4ac610ef7a18f4c2af3
Artic <81034458+ArticOff@users.noreply.github.com>
2022-07-08 18:27:50 +0200

    Update discord_kairo.create.py

--- discord_kairo.create.py
+++ discord_kairo.create.py
@@ -1,3 +1,10 @@
 from discord_kairo import CreateCommand
 
 CreateCommand('eval', 'dev', 'éxecuter des commandes', ['exe'])
+
+"""
+1. le nom de la commande (et du fichier)
+2. la catégorie (moderation, utility, fun)
+3. la description
+4. les aliases
+"""
