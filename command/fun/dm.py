9790d5d3d9e6b7476b06f07d46299147bcffd52d
Artic <81034458+ArticOff@users.noreply.github.com>
2022-07-08 20:15:26 +0200

    Update dm.py

--- command/fun/dm.py
+++ command/fun/dm.py
@@ -1,14 +1,13 @@
 from importlib import machinery
-discord_kairo = machinery.SourceFileLoader('discord_kairo','discord_kairo.py').load_module()
+discord_akerno = machinery.SourceFileLoader('discord_akerno','discord_akerno.py').load_module()
 
-async def _dm(bot, message: discord_kairo.ext.Message):
+async def _dm(bot, message: discord_akerno.ext.Message):
     await message.delete()
     await message.channel.send(f'{message.author.mention} has been dm!')
     await message.author.send(str(message.content).removeprefix('!dm '))
 
 if __name__ == '__main__':
-    dm = discord_kairo.Command
+    dm = discord_akerno.Command
     dm.constructor(dm, 'dm', 'fun', 'DM', ['mp'])
     dm.execute(dm, _dm)
     dm.Push(dm)
-    
