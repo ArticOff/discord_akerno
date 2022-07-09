279b9786c05e25dacf7518abc926f5b22c0499cb
Artic <81034458+ArticOff@users.noreply.github.com>
2022-07-09 14:42:10 +0200

    Update discord_akerno.create.py

--- discord_akerno.create.py
+++ discord_akerno.create.py
@@ -1,10 +1,14 @@
-from discord_akerno import CreateCommand
+from discord_akerno import CreateCommand, ext
 
-CreateCommand('eval', 'dev', 'éxecuter des commandes', ['exe'])
+Permission = ext.Permission
+
+CreateCommand('clear', 'moderation', 'efface des messages', ['clear'], Permission.gerer_les_messages, Permission.gerer_les_messages)
 
 """
 1. le nom de la commande (et du fichier)
 2. la catégorie (moderation, utility, fun)
 3. la description
 4. les aliases
+5. les permissions requises pour l'utilisateur
+6. les permissions requises pour le bot
 """
