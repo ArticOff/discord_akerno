fe0d93c7ea5eab959c9ed34061c5ec25a2b9a3cb
Artic <81034458+ArticOff@users.noreply.github.com>
2022-07-09 14:42:57 +0200

    Update coucou.py

--- command/fun/coucou.py
+++ command/fun/coucou.py
@@ -2,11 +2,11 @@
 from importlib import machinery
 discord_akerno = machinery.SourceFileLoader('discord_akerno','discord_akerno.py').load_module()
 
-async def _coucou(bot ,message: discord_akerno.ext.Message):
+async def _coucou(bot, message: discord_akerno.ext.Message):
     await message.reply('Coucou ! :wave:')
 
 if __name__ == '__main__':
     coucou = discord_akerno.Command
-    coucou.constructor(coucou, "coucou", "fun", "hey", ['salut', 'hey'])
+    coucou.constructor(coucou, "coucou", "fun", "hey", ['salut', 'hey'], None, None)
     coucou.execute(coucou, _coucou)
     coucou.Push(coucou)
