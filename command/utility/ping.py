6446033fdd51875994759d12c4d26a7583e78d23
Artic <81034458+ArticOff@users.noreply.github.com>
2022-07-09 14:45:24 +0200

    Update ping.py

--- command/utility/ping.py
+++ command/utility/ping.py
@@ -1,13 +1,12 @@
-from discord.ext import commands
-
 from importlib import machinery
 discord_akerno = machinery.SourceFileLoader('discord_akerno','discord_akerno.py').load_module()
 
-async def _ping(bot: commands.Bot, message: discord_akerno.ext.Message):
-    await message.reply(f'Pong ! :ping_pong:\n`{round(bot.latency * 1000, 3)}`ms')
+async def _say(bot, message: discord_akerno.ext.Message):
+    await message.delete()
+    await message.channel.send(str(message.content).removeprefix('!say '))
 
 if __name__ == '__main__':
-    ping = discord_akerno.Command
-    ping.constructor(ping, "ping", "utility", "Le ping du bot", ['ping', 'ms', 'latency'], None, None)
-    ping.execute(ping, _ping)
-    ping.Push(ping)
+    say = discord_akerno.Command
+    say.constructor(say, 'say', 'utility', 'Say', ['dire'], discord_akerno.ext.Permission.gerer_les_messages, None)
+    say.execute(say, _say)
+    say.Push(say)
