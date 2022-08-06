09f5b0139c0c9afdca06208ff2cff29404bd5c11
Artic <81034458+ArticOff@users.noreply.github.com>
2022-08-06 09:49:04 +0200

    Update discord_akerno.create.event.py

--- discord_akerno.create.event.py
+++ discord_akerno.create.event.py
@@ -3,11 +3,10 @@ from discord_akerno import CreateEvent, ext
 EventName = ext.EventName
 Arg = ext.EventArg
 
-CreateEvent("on_ready", "client", EventName.ready, [])
+CreateEvent(EventName.commandNotFound, "command", [Arg.context, Arg.error])
 
 """
-1. le nom de la commande (et du fichier)
+1. le nom de l'event' (et du fichier)
 2. la catégorie (client, command)
-3. le nom de l'event
-4. les arguments
+3. les arguments
 """
